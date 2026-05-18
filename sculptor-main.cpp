64f124033ba2eb92f547d7764e342c2ac6c84c65
julio cesar <julio.c.s.o.costa@gmail.com>
2026-05-18 00:25:01 -0300

    adicionados testes com mensagens de texto no terminal (provisório até ser implementado o writeOFF)

--- sculptor-main.cpp
+++ sculptor-main.cpp
@@ -3,13 +3,25 @@
 using namespace std;
 
 int main(void){
-    // sculptor-main por enquanto sera um arquivo de teste, seguindo o exemplo 'trono' do professor
+    // sculptor-main por enquanto sera um arquivo de teste
 
-    // cria um escultor cuja matriz tem 10x10x10 voxels (testa o construtor da classe)
-    Sculptor trono(10, 10, 10);
+    // teste do construtor da classe
+    Sculptor escultor(10, 10, 10);
 
-    // para mudar a cor do voxel (testa o metodo setColor)
-    trono.setColor(0,0,1.0,1.0); // cor atual definida como azul
-    
+    // teste do metodo setColor
+    escultor.setColor(0,0,1.0,1.0);
+
+    // testes do putVoxel fora dos limites da matriz
+    escultor.putVoxel(12, 10, 9);
+    escultor.putVoxel(-1, 10, 10);
+
+    // testes do putVoxel dentro dos limites da matriz
+    escultor.putVoxel(9, 9, 9);
+    escultor.putVoxel(3, 5, 9);
+
+    // testes do cutVoxel fora dos limites da matriz
+    escultor.cutVoxel(10, 20, 1);
+    escultor.cutVoxel(3, 5, 9);
+
     return 0;
 }
