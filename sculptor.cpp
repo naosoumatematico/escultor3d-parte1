3bd378b42ac5b3bd2f15d82afe6de19d58321ffe
julio cesar <julio.c.s.o.costa@gmail.com>
2026-05-17 23:28:37 -0300

    implementação do destrutor da classe

--- sculptor.cpp
+++ sculptor.cpp
@@ -11,22 +11,22 @@ Sculptor::Sculptor(int _nx, int _ny, int _nz){
 
     // processo de alocacao dinamica da matriz 3D
 
-    // v passa a apontar para uma regiao de memoria que pode conter 'nx' ponteiro para ponteiro para Voxel 
+    // alocacao do eixo x
     v = new Voxel** [nx];
 
-    // laco for percorre todos os elementos v[i] e eles passam a apontar para regioes de memoria que podem conter cada uma 'ny' ponteiros para Voxel
+    // alocacao do eixo y; 
     for(int i = 0; i < nx; i++){
         v[i] = new Voxel* [ny];
     }
 
-    // lacos for aninhados percorres v[i][j] e eles passam a apontar para regioes de memoria que podem conter cada uma 'nz' Voxel
+    // alocacao do eixo z
     for(int i = 0; i < nx; i++){
         for(int j = 0; j < ny; j++){
             v[i][j] = new Voxel [nz];
         }
     }
 
-    // lacos for aninhados para desativar os voxels
+    // os voxels sao inicialmente desativados
     for(int i = 0; i < nx; i++){
         for(int j = 0; j < ny; j++){
             for(int k = 0; k < nz; k++){
@@ -34,4 +34,25 @@ Sculptor::Sculptor(int _nx, int _ny, int _nz){
             }
         }
     }
+}
+
+// implementacao do destrutor da classe
+Sculptor::~Sculptor(){
+    // no destrutor, a memoria alocada deve ser liberada
+    // o processo acontece "de tras pra frente"
+
+    // liberacao do eixo z
+    for(int i = 0; i < nx; i++){
+        for(int j = 0; j < ny; j++){
+            delete[] v[i][j];
+        }
+    }
+
+    // liberacao do eixo y
+    for(int i = 0; i < nx; i++){
+        delete[] v[i];
+    }
+
+    // liberacao do eixo x
+    delete[] v;
 }
