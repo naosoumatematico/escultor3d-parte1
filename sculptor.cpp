2b6093c2570272128dc9c410f126a7f538bb0d77
julio cesar <julio.c.s.o.costa@gmail.com>
2026-05-17 23:46:42 -0300

    implementação do método setColor

--- sculptor.cpp
+++ sculptor.cpp
@@ -1,5 +1,4 @@
 // arquivo de definicao da classe; aqui ocorre a implementacao dos metodos da classe
-
 #include "sculptor.h" // inclusao do header da classe
 
 //implementacao do construtor da classe
@@ -55,4 +54,13 @@ Sculptor::~Sculptor(){
 
     // liberacao do eixo x
     delete[] v;
+}
+
+// implementacao do metodo que define a cor atual do desenho
+void Sculptor::setColor(float r, float g, float b, float alpha){
+    // usando this pois os argumentos tem os mesmos nomes que os atributos privados da classe
+    this->r = r;
+    this->g = g;
+    this->b = b;
+    this->a = alpha;
 }
