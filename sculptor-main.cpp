83b3ed4c255ec10a1c68e36c2251780a33b67b14
julio cesar <julio.c.s.o.costa@gmail.com>
2026-05-20 14:37:35 -0300

    implementação completa do método writeOFF + testes

--- sculptor-main.cpp
+++ sculptor-main.cpp
@@ -1,19 +1,118 @@
-#include <iostream>
+// programa de teste para a classe Sculptor. o teste sera feito com 2 desenhos:
+// uma pixel art de coracao tirada do minecraft 
+// o trono.off do site do professor sobre o projeto
+
 #include "sculptor.h"
-using namespace std;
 
 int main(void){
-    Sculptor teste(11, 11, 11);
+    // construtor da classe criando uma matriz do tamanho necessario para uma pixel art de coracao
+    Sculptor heart(15, 14, 1);
 
-    teste.setColor(1, 0, 0, 1);
+    // seta a cor preta para desenhar o contorno
+    heart.setColor(0.0, 0.0, 0.0, 1.0);
+    
+    // ===============================
+    // desenho do contorno do coracao
+    // ===============================
 
-    teste.putVoxel(10, 10, 10);
+    // desenha a subida e a descida da parte inferior em um loop so
+    for(int x = 0; x < 7; x++){
+        heart.putVoxel(x, 7 - x, 0);
+        heart.putVoxel(x + 8, x + 1, 0);
+    }
 
-    teste.cutVoxel(10, 10, 10);
+    // desenha a pontinha
+    heart.putVoxel(7, 0, 0);
 
-    teste.putBox(0, 9, 0, 9, 0, 9);
+    // desenha as retas verticais que ligam a parte inferior a superior em um loop so
+    for (int y = 8; y < 12; y++){
+        heart.putVoxel(0, y, 0);
+        heart.putVoxel(14, y, 0);
+    }
 
-    teste.writeOFF("teste.off");
+    // desenha as retas horizontais da parte superior em um loop so
+    for(int x = 2; x < 6; x++){
+        heart.putVoxel(x, 13, 0);
+        heart.putVoxel(x + 7, 13, 0);
+    }
 
+    // desenha os voxels faltantes
+    heart.putVoxel(1, 12, 0);
+    heart.putVoxel(6, 12, 0);
+    heart.putVoxel(7, 11, 0);
+    heart.putVoxel(8, 12, 0);
+    heart.putVoxel(13, 12, 0);
+
+    // ===============================
+    // pintura do interior do coracao
+    // ===============================
+
+    // seta a cor vermelha para o interior do coracao
+    heart.setColor(1.0, 0.0, 0.0, 1.0);
+
+    for(int x = 2; x < 6; x++){
+        heart.putVoxel(x, 12, 0);
+        heart.putVoxel(x + 7, 12, 0);
+    }
+
+    for(int x = 1; x < 7; x++){
+        heart.putVoxel(x, 11, 0);
+        heart.putVoxel(x + 7, 11, 0);
+    }
+
+    heart.putBox(1, 13, 7, 10, 0, 0);
+
+    for(int x = 2; x < 13; x++){
+        heart.putVoxel(x, 6, 0);
+    }
+
+    for(int x = 3; x < 12; x++){
+        heart.putVoxel(x, 5, 0);
+    }
+
+    for(int x = 4; x < 11; x++){
+        heart.putVoxel(x, 4, 0);
+    }
+
+    for(int x = 5; x < 10; x++){
+        heart.putVoxel(x, 3, 0);
+    }
+
+    for(int x = 6; x < 9; x++){
+        heart.putVoxel(x, 2, 0);
+    }
+
+    heart.putVoxel(7, 1, 0);
+
+    // ===============================
+    // pintura do ponto luminoso no coracao
+    // ===============================
+
+    heart.setColor(1.0, 1.0, 1.0, 1.0);
+
+    for(int y = 9; y < 11; y++){
+        heart.putVoxel(2, y, 0);
+        heart.putVoxel(3, y + 1, 0);
+    }
+
+    heart.putVoxel(4, 11, 0);
+
+    // ===============================
+    // criacao e salvamento do arquivo .off com o desenho
+    // ===============================
+
+    heart.writeOFF("heart.off");
+
+    // cria um escultor cuja matriz tem 10x10x10 voxels
+    Sculptor trono(10,10,10);
+    // para mudar a cor do voxel
+    trono.setColor(0,0,1.0,1.0); // azul
+    // ativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
+    trono.putBox(0,9,0,9,0,9);
+    // desativa os voxels na faixa de [x,y,z] pertencendo a [0-9]
+    trono.cutBox(1,8,1,9,1,9);
+    // grava a escultura digital no arquivo "trono.off"
+    trono.writeOFF("trono.off");
+    
     return 0;
 }
--- sculptor.cpp
+++ sculptor.cpp
@@ -1,13 +1,12 @@
 // arquivo de definicao da classe; aqui ocorre a implementacao dos metodos da classe
 
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include "sculptor.h"
+#include <fstream> // para criar e escrever no arquivo .off que sera gerado
+#include <cstdlib> // para usar exit(1)
+#include "sculptor.h" // header da classe sculptor
 
 using namespace std;
 
-// implementacao do construtor da classe
+// construtor da classe
 Sculptor::Sculptor(int _nx, int _ny, int _nz){
     // definicao das dimensoes da matriz 3D
 	nx = _nx;
@@ -15,8 +14,10 @@ Sculptor::Sculptor(int _nx, int _ny, int _nz){
 	nz = _nz;
 
     // alocacao dinamica da matriz
+
 	v = new Voxel**[nx];
-	for(int i = 0; i < nx; i++){
+	
+    for(int i = 0; i < nx; i++){
 		v[i] = new Voxel*[ny];
 		for(int j = 0; j < ny; j++){
 			v[i][j] = new Voxel[nz];
@@ -27,7 +28,7 @@ Sculptor::Sculptor(int _nx, int _ny, int _nz){
 	}
 }
 
-// implementacao do destrutor da classe
+// destrutor da classe
 Sculptor::~Sculptor(){
     // como a classe trabalha com alocacao dinamica de memoria, o construtor deve se encarregar de liberar a memoria alocada
     // a liberacao deve ser feita "na ordem contraria" da alocacao, a fim de evitar memory leak
@@ -41,6 +42,8 @@ Sculptor::~Sculptor(){
 }
 
 // define a cor atual do desenho
+// this eh usado pois os parametros rgb do argumento tem o mesmo nome que os atributos da classe
+// ou seja, r = r nao faria sentido;
 void Sculptor::setColor(float r, float g, float b, float alpha){
     this->r = r;
     this->g = g;
@@ -55,7 +58,7 @@ void Sculptor::putVoxel(int x, int y, int z){
         return; // se tentar acessar uma regiao fora da matriz, o metodo nao faz nada
     }
 
-    // ativa o voxel com a cor definida no setColor
+    // se a coordenada for valida, ativa o voxel com a cor definida no setColor
     v[x][y][z].show = true;
     v[x][y][z].r = r;
     v[x][y][z].g = g;
@@ -70,7 +73,7 @@ void Sculptor::cutVoxel(int x, int y, int z){
         return; // se tentar acessar uma regiao fora da matriz, o metodo nao faz nada
     }
 
-    // desativa o voxel
+    // se a coordenada for valida, desativa o voxel
     v[x][y][z].show = false;
 }
 
@@ -79,7 +82,7 @@ void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
     for(int x = x0; x <= x1; x++){
         for(int y = y0; y <= y1; y++){
             for(int z = z0; z <= z1; z++){
-                putVoxel(x, y, z); // os lacos de repeticao percorrem o intervalo, e ativam o voxel onde for valido
+                putVoxel(x, y, z); // os lacos de repeticao percorrem o intervalo, e ativam o voxel onde for valido chamando o metodo putVoxel
             }
         }
     }
@@ -90,39 +93,104 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
     for(int x = x0; x <= x1; x++){
         for(int y = y0; y <= y1; y++){
             for(int z = z0; z <= z1; z++){
-                cutVoxel(x, y, z); // os lacos de repeticao percorrem o intervalo, e desativam o voxel onde for valido
+                cutVoxel(x, y, z); // os lacos de repeticao percorrem o intervalo, e desativam o voxel onde for valido chamando o metodo cutVoxel
             }
         }
     }
 }
 
 // salva o desenho em um arquivo OFF
-void Sculptor::writeOFF(const char* filename)
-{
-   ofstream fout;
-   fout.open(filename);
+void Sculptor::writeOFF(const char* filename){
+   ofstream fout; // objeto da classe ofstream para escrever no arquivo
+   fout.open(filename); // metodo open tenta abrir o arquivo
    if(!fout.is_open()){
     exit(1);
    }
 
+   // variavel acumuladora inteira para contar o total de voxels no desenho
+   // deve ser inicializada com valor 0 para que o lixo nao interfira na contagem
    int nVoxels = 0;
 
+   // lacos de repeticao aninhados que percorrem toda a matriz 3D contando o total de voxels
    for(int x = 0; x < nx; x++){
         for(int y = 0; y < ny; y++){
             for(int z = 0; z < nz; z++){
-                if(v[x][y][z].show == true){
+                if(v[x][y][z].show){
                     nVoxels++;
                 }
             }
         }
     }
 
-    int nFaces = nVoxels*6;
-    int nVertices = nVoxels*8;
-
+    // escreve o header do arquivo
+    /* exemplo:
+       OFF
+       8 6 0
+       cria um arquivo .off que comporta 8 vertices e 6 faces */
     fout << "OFF\n";
-    fout << nVertices << " " << nFaces << " 0\n";
+    fout << nVoxels*8 << " " << nVoxels*6 << " 0\n"; /* como vamos desenhar apenas cubos, multiplicamos nVoxels por 8 para saber o 
+                                                        total de vertices e por 6 para saber o total de faces */
+                                                        
 
+    // escreve uma lista com todos os vertices presentes no desenho
+    for(int x = 0; x < nx; x++){
+        for(int y = 0; y < ny; y++){
+            for(int z = 0; z < nz; z++){
+                if(v[x][y][z].show){ // aqui estou considerando (x, y, z) como a coordenada no centro do voxel
+
+                    /* considerando o voxel como sendo 1x1x1 e (x, y, z) como o centro, cada vertice eh deslocado em 0.5 em cada
+                       dimensao com relacao ao seu centro 
+                       eh util visualizar como uma tabela verdade
+                       cada linha de fout imprime uma linha com as coordenadas de cada um dos vertices
+                       a sequencia de fout se repete para cada v[x][y][z].show verdadeiro*/
+                    
+                                                                                 // indices de cada vertice
+                    fout << x - 0.5 << " " << y - 0.5 << " " << z - 0.5 << "\n"; // v0
+                    fout << x + 0.5 << " " << y - 0.5 << " " << z - 0.5 << "\n"; // v1
+                    fout << x - 0.5 << " " << y + 0.5 << " " << z - 0.5 << "\n"; // v2
+                    fout << x + 0.5 << " " << y + 0.5 << " " << z - 0.5 << "\n"; // v3
+                    fout << x - 0.5 << " " << y - 0.5 << " " << z + 0.5 << "\n"; // v4
+                    fout << x + 0.5 << " " << y - 0.5 << " " << z + 0.5 << "\n"; // v5
+                    fout << x - 0.5 << " " << y + 0.5 << " " << z + 0.5 << "\n"; // v6
+                    fout << x + 0.5 << " " << y + 0.5 << " " << z + 0.5 << "\n"; // v7
+                }
+            }
+        }
+    }
+
+    /* escreve uma lista onde cada linha contem a conexao de vertices que forma uma face, pintando-a com a cor definida para cada voxel
+       para idealizar essa lista, desenhei um cubo e atribui para cada vertice indices de 0 a 7 e fiz as conexoes que desenham
+       corretamente as faces de um cubo no formato pedido pelo OFF */
+
+    // variavel que armazena o indice de cada vertice
+    int vertice = 0;
+
+    for(int x = 0; x < nx; x++){
+        for(int y = 0; y < ny; y++){
+            for(int z = 0; z < nz; z++){
+                if(v[x][y][z].show){ // lacos aninhados que percorrem toda a matriz procurando voxels ativos
+                    
+                    /* escrita no arquivo ocorre na forma:
+                       nVertices v0 v1 v3 v2 r g b a
+                       onde nVertices eh o numero de vertices a serem ligadas para desenhar uma face (no caso dos cubos, sao sempre 4)
+                       v0 - v2 sao os indices dos vertices seguindo uma ordem que complete a face
+                       r, g, b, a sao as cores e opacidade definidas no setColor
+                        */
+                    fout << 4 << " " << vertice + 0 << " " << vertice + 1 << " " << vertice + 3 << " " << vertice + 2 << " " << v[x][y][z].r << " " << v[x][y][z].g << " " << v[x][y][z].b << " " << v[x][y][z].a << "\n";
+                    fout << 4 << " " << vertice + 0 << " " << vertice + 1 << " " << vertice + 5 << " " << vertice + 4 << " " << v[x][y][z].r << " " << v[x][y][z].g << " " << v[x][y][z].b << " " << v[x][y][z].b << "\n";
+                    fout << 4 << " " << vertice + 0 << " " << vertice + 2 << " " << vertice + 6 << " " << vertice + 4 << " " << v[x][y][z].r << " " << v[x][y][z].g << " " << v[x][y][z].b << " " << v[x][y][z].b << "\n";
+                    fout << 4 << " " << vertice + 3 << " " << vertice + 1 << " " << vertice + 5 << " " << vertice + 7 << " " << v[x][y][z].r << " " << v[x][y][z].g << " " << v[x][y][z].b << " " << v[x][y][z].b << "\n";
+                    fout << 4 << " " << vertice + 3 << " " << vertice + 2 << " " << vertice + 6 << " " << vertice + 7 << " " << v[x][y][z].r << " " << v[x][y][z].g << " " << v[x][y][z].b << " " << v[x][y][z].b << "\n";
+                    fout << 4 << " " << vertice + 4 << " " << vertice + 5 << " " << vertice + 7 << " " << vertice + 6 << " " << v[x][y][z].r << " " << v[x][y][z].g << " " << v[x][y][z].b << " " << v[x][y][z].b << "\n";
+
+                    // apos o fim da escrita de um Voxel, vertice eh somado 8 para continuar no proximo voxel
+                    vertice += 8;
+                }
+            }
+        }
+    }
+
+    // fecha o arquivo apos fazer todas as alteracoes
     fout.close();
     
 }
